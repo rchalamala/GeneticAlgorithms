6ced9b65e638f6874727b22b270b0f1872500558
Rahul Chalamala <rchalamala@outlook.com>
2021-09-09 22:35:34 -0600

    sudoku updates

--- Gradient Descent/main.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep  7 09:18:14 2021
-
-@author: Rahul Chalamala
-"""
-
--- Sudoku/main.py
+++ Sudoku/main.py
@@ -7,7 +7,6 @@ Created on Wed Sep  8 11:34:40 2021
 """
 
 import numpy as np
-import matplotlib.pyplot as plt
 import copy
 
 '''
@@ -20,76 +19,124 @@ rng = np.random.default_rng(seed)
 rng = np.random.default_rng()
 
 
-population_size = 1000
+population_size = 500
 generations = 10000
-crossover_probability = 0.95
-mutation_probability = 0.85
-elitism = 0.3
+crossover_probability = 0.7
+mutation_probability = 0.6
+elitism = 0.7
 
 
 n = 9
-blocks = 3
+blocksize = 3
 
-assert n % blocks == 0
+assert n % blocksize == 0
 
-blocksize = n // blocks
+
+def process(board, solution):
+    combined = copy.deepcopy(board)
+
+    for row in range(n):
+        replaced = 0
+
+        for column in range(n):
+            if combined[row][column] == 0:
+                combined[row][column] = solution[row][replaced]
+                replaced += 1
+
+    return combined
+
+
+def output(board):
+    for row in board:
+        for point in row:
+            print(point, end='')
+        print()
+    print()
+
+
+def generate_missing(goal):
+    return np.array([np.array(list(set(np.arange(1, n + 1)).difference(set(goal[row])))) for row in range(n)], dtype=object)
+
 
 '''
-goal = np.array([np.array([4,0,1,2,9,0,0,7,5]),
-                 np.array([2,0,0,3,0,0,8,0,0]),
-                 np.array([0,7,0,0,8,0,0,0,6]),
-                 np.array([0,0,0,1,0,3,0,6,2]),
-                 np.array([1,0,5,0,0,0,4,0,3]),
-                 np.array([7,3,0,6,0,8,0,0,0]),
-                 np.array([6,0,0,0,2,0,0,3,0]),
-                 np.array([0,0,7,0,0,1,0,0,4]),
-                 np.array([8,9,0,0,6,5,1,0,7])], dtype=object)
+def generate_bounds(goal, missing):
+    bounds = []
+    for row in range(n):
+        bound = [0]
+        total = 0
+        for column in range(n):
+            if column and column % blocksize == 0:
+                bound.append(total)
+            if goal[row, column] == 0:
+                total += 1
+        bound.append(total)
+        bounds.append(np.array(bound))
+    return np.array(bounds, dtype=object)
 '''
-#'''
-goal = np.array([np.array([1,0,0,0,0,7,0,9,0]),
-                 np.array([0,3,0,2,0,0,0,0,9]),
-                 np.array([0,0,9,6,0,0,5,0,0]),
-                 np.array([0,0,5,3,0,0,9,0,0]),
-                 np.array([0,1,0,0,8,0,0,0,2]),
-                 np.array([6,0,0,0,0,4,0,0,0]),
-                 np.array([3,0,0,0,0,0,0,1,0]),
-                 np.array([0,4,0,0,0,0,0,0,7]),
-                 np.array([0,0,7,0,0,0,3,0,0])], dtype=object)
-#'''
-
-missing = []
-
-for row in range(n):
-    used = set(goal[row])
-    length = n - len(used) + (0 in used)
-    number = 1
-    values = []
-    while length:
-        if number not in used:
-            length -= 1
-            values.append(number)
-        number += 1
-    missing.append(np.array(values))
-
-missing = np.array(missing, dtype=object)
 
 
-def initialize(size):
+def presolve(board):
+    rows = [set(np.arange(1, n + 1)) for _ in range(n)]
+    columns = [set(np.arange(1, n + 1)) for _ in range(n)]
+    blocks = [set(np.arange(1, n + 1)) for _ in range(n)]
+
+    for row in range(n):
+        for column in range(n):
+            if board[row, column] > 0:
+                rows[row].remove(board[row, column])
+                columns[column].remove(board[row, column])
+                blocks[3 * (row // 3) + column // 3].remove(board[row, column])
+
+    row = 0
+
+    while row < n:
+        column = 0
+
+        while column < n:
+            block = 3 * (row // 3) + column // 3
+
+            available = rows[row].union(columns[column].union(blocks[block]))
+
+            if len(available) == 1:
+                item = list(available[0])
+
+                board[row, column] = item
+
+                rows[row].remove(item)
+                columns[column].remove(item)
+                blocks[block].remove(item)
+
+                break
+
+            column += 1
+
+        if column < n:
+            row = 0
+
+        else:
+            row += 1
+
+
+def initialize(missing):
     population = []
 
-    for _ in range(size):
+    for _ in range(population_size):
         individual = copy.deepcopy(missing)
-        for row in range(n):    
+
+        for row in range(n):
             rng.shuffle(individual[row])
+
         population.append(individual)
 
     return population
 
 
-def fitness(solution):
+def fitness(solution, goal):
     combined = copy.deepcopy(goal)
+
     for row in range(n):
         replaced = 0
+
         for column in range(n):
             if combined[row][column] == 0:
                 combined[row][column] = solution[row][replaced]
@@ -98,11 +145,11 @@ def fitness(solution):
     total = 0
 
     for iterator in range(n):
-        total += 2 * n - len(set(combined[iterator])) - len(set(combined[:,iterator]))
+        total += 2 * n - len(set(combined[iterator])) - len(set(combined[:, iterator]))
 
-    for row in range(blocks):
-        for column in range(blocks):
-            total += blocksize * blocksize - len(set(combined[blocksize * row:blocksize * (row + 1), blocksize * column:blocksize * (column + 1 )].flatten()))
+    for row in range(blocksize):
+        for column in range(blocksize):
+            total += blocksize * blocksize - len(set(combined[blocksize * row:blocksize * (row + 1), blocksize * column:blocksize * (column + 1)].flatten()))
 
     return total
 
@@ -111,11 +158,45 @@ def selection(population, weights):
     return rng.choice(population, p=weights)
 
 
+'''
+def crossover(parent1, parent2, bounds):
+    parent1Blocks = rng.choice(np.arange(0, n), np.random.randint(1, n))
+
+    if np.random.randint(0, 2):
+        parent1, parent2 = parent2, parent1
+
+    child1 = copy.deepcopy(parent2)
+    child2 = copy.deepcopy(parent1)
+
+    for block in parent1Blocks:
+        for row in range(blocksize):
+            here = blocksize * (block // blocksize) + row
+            column = block % blocksize
+
+            child1[here][bounds[here][column]:bounds[here][column + 1]] = parent1[here][bounds[here][column]:bounds[here][column + 1]]
+            child2[here][bounds[here][column]:bounds[here][column + 1]] = parent2[here][bounds[here][column]:bounds[here][column + 1]]
+
+    return [child1, child2]
+
+
+def mutation(individual, bounds):
+    row = np.random.randint(0, n)
+    
+    mutant = copy.deepcopy(individual)
+    
+    swap = np.random.randint(0, len(bounds[row]) - 1)
+
+    rng.shuffle(mutant[row][bounds[row][swap]:bounds[row][swap + 1]])
+
+    return mutant
+'''
+
+
 def crossover(parent1, parent2):
     c1 = np.random.randint(0, n)
     c2 = np.random.randint(c1 + 1, n + 1)
 
-    child = copy.deepcopy(missing)
+    child = copy.deepcopy(parent1)
 
     child[:c1] = parent2[:c1]
 
@@ -140,13 +221,44 @@ def mutation(individual):
 
 
 def main():
-    population = initialize(population_size)
+
+    # '''
+    goal = np.array([np.array([4,0,1,2,9,0,0,7,5]),
+                     np.array([2,0,0,3,0,0,8,0,0]),
+                     np.array([0,7,0,0,8,0,0,0,6]),
+                     np.array([0,0,0,1,0,3,0,6,2]),
+                     np.array([1,0,5,0,0,0,4,0,3]),
+                     np.array([7,3,0,6,0,8,0,0,0]),
+                     np.array([6,0,0,0,2,0,0,3,0]),
+                     np.array([0,0,7,0,0,1,0,0,4]),
+                     np.array([8,9,0,0,6,5,1,0,7])], dtype=object)
+     
+    '''
+    goal = np.array([np.array([1,0,0,0,0,7,0,9,0]),
+                     np.array([0,3,0,2,0,0,0,0,8]),
+                     np.array([0,0,9,6,0,0,5,0,0]),
+                     np.array([0,0,5,3,0,0,9,0,0]),
+                     np.array([0,1,0,0,8,0,0,0,2]),
+                     np.array([6,0,0,0,0,4,0,0,0]),
+                     np.array([3,0,0,0,0,0,0,1,0]),
+                     np.array([0,4,0,0,0,0,0,0,7]),
+                     np.array([0,0,7,0,0,0,3,0,0])], dtype=object)
+    '''
+
+    presolve(goal)
+
+    missing = generate_missing(goal)
+
+    # bounds = generate_bounds(goal, missing)
+
+    population = initialize(missing)
+
     solution = [0, np.empty(n)]
 
     for i in range(generations):
         print(i)
 
-        population_fitness = [fitness(x) for x in population]
+        population_fitness = [fitness(x, goal) for x in population]
 
         best_fitness = min(population_fitness)
         best_index = population_fitness.index(best_fitness)
@@ -155,23 +267,23 @@ def main():
         if i == 0 or best_fitness < solution[0]:
             solution = [best_fitness, best_individual]
 
-        if max(population_fitness) - min(population_fitness) < 1e-7 or solution[0] == 0:
+        if max(population_fitness) == min(population_fitness) or solution[0] == 0:
             if solution[0] == 0:
                 print("CONVERGED")
+
                 break
+
             else:
                 print("CONVERGED AND RESTARTING")
                 
-                population = initialize(population_size)
-                
-                i -= 1
+                population = initialize(missing)
+                i = -1
+
                 continue
 
         print(max(population_fitness))
         print(min(population_fitness))
         print(solution[0])
-
-        new_population = []
 
         max_value = max(population_fitness)
         total_sum = max_value * population_size - sum(population_fitness)
@@ -184,49 +296,29 @@ def main():
             parent1 = selection(nppopulation, weights)
             parent2 = selection(nppopulation, weights)
 
-
             if np.random.rand() <= crossover_probability:
                 child = crossover(parent1, parent2)
+
             else:
                 child = rng.choice(np.array([parent1, parent2]))
 
             if np.random.rand() <= mutation_probability:
                 child = mutation(child)
 
-            new_population.append(child)
-
-        population.extend(new_population)
-
-        population_fitness.extend([fitness(x) for x in new_population])
+            population.append(child)
+            population_fitness.append(fitness(child, goal))
 
         population = [x for _, x in sorted(zip(population_fitness, population), key=lambda pair: pair[0])]
 
         elite = population[:int(elitism * population_size)]
         normal = rng.choice(np.array(population[int(elitism * population_size):], dtype=object), population_size - int(elitism * population_size)).tolist()
-        
+
         population = elite
         population.extend(normal)
 
-    for row in goal:
-        for point in row:
-            print(point, end=' ')
-        print()
-    print()
+    output(goal)
+    output(process(goal, solution[1]))
 
-    combined = copy.deepcopy(goal)
-    for row in range(n):
-        replaced = 0
-        for column in range(n):
-            if combined[row][column] == 0:
-                combined[row][column] = solution[1][row][replaced]
-                replaced += 1
-
-    for row in combined:
-        for point in row:
-            print(point, end=' ')
-        print()
-    print()
-    
     print(solution[0])
 
 
