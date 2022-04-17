c33710645805235e9ee094e151bcf95eea5c6d67
rchalamala <rchalamala@outlook.com>
2022-04-17 16:34:07 -0600

    numpy everything

--- Traveling Salesman Problem/main.py
+++ Traveling Salesman Problem/main.py
@@ -10,7 +10,7 @@ Created on Sun Sep  5 21:30:31 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-
+import time
 
 rng = np.random.default_rng()
 
@@ -18,13 +18,13 @@ rng = np.random.default_rng()
 n = 30
 
 lb = 0
-ub = 100
+ub = 1000
 
-population_size = 30
-generations = 100
+population_size = 100
+generations = 1000
 crossover_probability = 0.65
-mutation_probability = 0.3
-elitism = 0.2
+mutation_probability = 0.4
+elitism = 0.8
 
 
 x = np.random.randint(lb, ub, n)
@@ -32,31 +32,20 @@ y = np.random.randint(lb, ub, n)
 city_map = np.c_[x, y]
 
 
-plt.figure()
-plt.scatter(x, y)
+dt = np.dtype('u1')
+fdt = np.dtype('f8')
 
-
-cities = np.arange(n)
+cities = np.arange(n, dtype=dt)
 
 
 def initialize():
-    population = []
-
-    for _ in range(population_size):
-        individual = cities.copy()
-        rng.shuffle(individual)
-        population.append(individual)
-
-    return population
+    population = np.full(shape=(population_size, n), fill_value=cities, dtype=object)
+    
+    return rng.permuted(population, axis=1)
 
 
 def fitness(solution):
-    total = 0
-
-    for i in range(n):
-        total += np.linalg.norm(city_map[solution[i]] - city_map[solution[(i + 1) % n]])
-
-    return total
+    return np.sum([np.linalg.norm(city_map[solution[i]] - city_map[solution[(i + 1) % n]]) for i in range(n)], dtype=fdt)
 
 
 def selection(population, weights):
@@ -64,44 +53,57 @@ def selection(population, weights):
 
 
 def crossover(parent1, parent2):
-    c1 = np.random.randint(0, n)
-    c2 = np.random.randint(c1 + 1, n + 1)
+    c1 = np.random.randint(0, n, dtype=dt)
+    c2 = np.random.randint(c1 + 1, n + 1, dtype=dt)
 
-    child = np.full(n, -1)
-
+    child = np.full(n, n, dtype=dt)
+    
     child[c1:c2] = parent1[c1:c2]
 
-    # BINARY SEARCH TO CHECK IF CHILD IN PARENT
-
+    values = np.sort(child[c1:c2])
+    values_size = len(values)
+    
     j = 0
     for i in range(c1):
-        while parent2[j] in child:
-            j += 1
+        while True:
+            index = np.searchsorted(values, parent2[j])
+            if index == values_size or (values[index] != parent2[j]):
+                break
+            else:
+                j += 1
+
         child[i] = parent2[j]
+        j += 1
 
     for i in range(c2, len(parent2)):
-        while parent2[j] in child:
-            j += 1
+        while True: 
+            index = np.searchsorted(values, parent2[j])
+            if index == values_size or (values[index] != parent2[j]):
+                break
+            else:
+                j += 1
+        
         child[i] = parent2[j]
+        j += 1
 
     return child
 
 
 def mutation(individual):
-    i = np.random.randint(0, n)
-    j = np.random.randint(0, n)
+    i = np.random.randint(0, n, dtype=dt)
+    j = np.random.randint(0, n, dtype=dt)
 
-    mutant = individual.copy()
+    individual[i], individual[j] = individual[j], individual[i]
 
-    mutant[i], mutant[j] = mutant[j], mutant[i]
-
-    return mutant
+    return individual
 
 
 def main():
+    start = time.time()
+
     population = initialize()
-    population = [(fitness(organism), organism) for organism in population]
-    population.sort(key = lambda x: x[0])
+
+    population_fitness = np.array([fitness(organism) for organism in population], dtype=fdt)
 
     solution = [0, 0]
     original = 0
@@ -109,36 +111,34 @@ def main():
     for i in range(generations):
         print(i)
 
-        best_fitness = population[0][0]
-        best_index = 0
-        best_individual = population[0][1]
+        indices = population_fitness.argsort()
 
-        if i == 0 or best_fitness < solution[0]:
-            solution = [best_fitness, best_individual]
+        population_fitness = population_fitness[indices]
+        population = population[indices]
 
-        # print(population_fitness)
+        if i == 0 or population_fitness[0] < solution[0]:
+            solution = [population_fitness[0], population[0]]
+
         if i == 0:
-            original = population[0][0]
+            original = population[0]
 
-        if population[-1][0] - population[0][0] < 1e-2:
+        if population_fitness[-1] - population_fitness[0] < 1:
             print("CONVERGED")
             break
 
-        print(population[-1][0])
-        print(population[0][0])
-        print(solution[0])
+        print(population_fitness[0])
+        #print(solution[0])
 
-        new_population = []
+        max_value = population_fitness[-1]
+        total_sum = max_value * population_size - np.sum(population_fitness)
+        weights = np.array([(max_value - x) / total_sum for x in population_fitness], dtype=fdt)
 
-        max_value = population[-1][0]
-        total_sum = max_value * population_size - sum(x for x, organism in population)
-        weights = [(max_value - x) / total_sum for x, organism in population]
-
-        population_organisms = np.array([organism for x, organism in population], dtype=object)
+        new_population = np.zeros(shape=(population_size, n), dtype=dt)
+        new_population_fitness = np.zeros(population_size, dtype=fdt)
 
         for j in range(population_size):
-            parent1 = selection(population_organisms, weights)
-            parent2 = selection(population_organisms, weights)
+            parent1 = selection(population, weights)
+            parent2 = selection(population, weights)
 
             if np.random.rand() <= crossover_probability:
                 child = crossover(parent1, parent2)
@@ -148,28 +148,31 @@ def main():
             if np.random.rand() <= mutation_probability:
                 child = mutation(child)
 
-            new_population.append((fitness(child), child))
+            new_population[j] = child
+            new_population_fitness[j] = fitness(child)
 
         elite_population_size = int(elitism * population_size)
         
-        population = population[:elite_population_size]
+        normal = rng.choice(np.arange(population_size, dtype=dt), population_size - elite_population_size)
 
-        normal = rng.choice(np.array(new_population, dtype=object), population_size - elite_population_size).tolist()
+        population[int(elitism * population_size):] = new_population[normal]
+        population_fitness[int(elitism * population_size):] = new_population_fitness[normal]
+        
 
-        population.extend(normal)
-        population.sort(key = lambda x: x[0])
+    print(str(time.time() - start) + "s")
 
+    print(original)
+    print(solution[0])
+    
     solution_points = np.array([city_map[x] for x in solution[1]] + [city_map[solution[1][0]]])
     x = solution_points[:, 0]
     y = solution_points[:, 1]
 
     plt.figure()
-    plt.text(1, 1, fitness(solution[1]))
+    plt.text(1, 1, solution[0])
     plt.scatter(x, y)
     plt.plot(x, y)
-
-    print(original)
-    print(fitness(solution[1]))
+    plt.show()
 
 
 if __name__ == "__main__":
